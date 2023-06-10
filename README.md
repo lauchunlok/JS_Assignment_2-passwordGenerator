b3e6926067656b79bcd27d50393dff2d820c7d91
Chun Lok Lau <lauchunlok1@gmail.com>
2023-06-10 16:07:21 +0100

    Add 1 click copy

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
+> Coming back to this well-trodden exercise after ChatGPT was released, every TODO feature has become 10 times easier. Implementing new features, especially on this well-established project, can be accomplished with just 2-3 prompts.
+
 # JS_Assignment_2-passwordGenerator
 
 Design File: https://www.figma.com/file/YRO9Iw5IYaOorjnRyNz4bV/Random-Password-Generator?node-id=0%3A1
 
+Demo:
 
 ### TODO
-- 1-click copy password to the clipboard
-- More scalable on the number of password being generated (can be increase/decrease from 4 to 2/6)
-- The assignment leaves a lot of flexibility on design. If time permits try to stick to the design file as an exercise.
 
+-   ~~1-click copy password to the clipboard~~
+-   More scalable on the number of password being generated (can be increase/decrease from 4 to 2/6)
+-   The assignment leaves a lot of flexibility on design. If time permits try to stick to the design file as an exercise.
--- index.js
+++ index.js
@@ -1,37 +1,63 @@
-let AllChars = []
-let displayPassword1 = document.getElementById("displayPassword1")
-let displayPassword2 = document.getElementById("displayPassword2")
-let displayPassword3 = document.getElementById("displayPassword3")
-let displayPassword4 = document.getElementById("displayPassword4")
+let AllChars = [];
+let displayPassword1 = document.getElementById("displayPassword1");
+let displayPassword2 = document.getElementById("displayPassword2");
+let displayPassword3 = document.getElementById("displayPassword3");
+let displayPassword4 = document.getElementById("displayPassword4");
+const passwordElements = document.querySelectorAll(".displayPassword");
+const passwordLengthInput = document.getElementById("passwordLengthInput");
 
 for (let i = 32; i < 127; i++) {
-  AllChars.push(String.fromCharCode(i))
+    AllChars.push(String.fromCharCode(i));
 }
 
 function generatePassword() {
-    var passwordLength = document.getElementById("length").value
+    var passwordLength = passwordLengthInput.value;
 
-    let allCharcterLength = AllChars.length
-    let firstPassword = ""
-    let secondPassword = ""
-    let thirdPassword = ""
-    let fourthPassword = ""
-    
-    for (let j = 0; j < passwordLength; j++) {
-        firstPassword += AllChars[Math.floor(Math.random() * allCharcterLength)]
-        secondPassword += AllChars[Math.floor(Math.random() * allCharcterLength)]
-        thirdPassword += AllChars[Math.floor(Math.random() * allCharcterLength)]
-        fourthPassword += AllChars[Math.floor(Math.random() * allCharcterLength)]
+    if (isNaN(passwordLength)) {
+        alert("Please enter a valid number for the password length.");
+    } else {
+        let allCharcterLength = AllChars.length;
+        let firstPassword = "";
+        let secondPassword = "";
+        let thirdPassword = "";
+        let fourthPassword = "";
+
+        for (let j = 0; j < passwordLength; j++) {
+            firstPassword +=
+                AllChars[Math.floor(Math.random() * allCharcterLength)];
+            secondPassword +=
+                AllChars[Math.floor(Math.random() * allCharcterLength)];
+            thirdPassword +=
+                AllChars[Math.floor(Math.random() * allCharcterLength)];
+            fourthPassword +=
+                AllChars[Math.floor(Math.random() * allCharcterLength)];
+        }
+
+        // console.log(firstPassword)
+        // console.log(secondPassword)
+        // console.log(thirdPassword)
+        // console.log(fourthPassword)
+
+        displayPassword1.textContent = firstPassword;
+        displayPassword2.textContent = secondPassword;
+        displayPassword3.textContent = thirdPassword;
+        displayPassword4.textContent = fourthPassword;
     }
-    
-    // console.log(firstPassword)
-    // console.log(secondPassword)
-    // console.log(thirdPassword)
-    // console.log(fourthPassword)
-    
-    displayPassword1.textContent = firstPassword
-    displayPassword2.textContent = secondPassword
-    displayPassword3.textContent = thirdPassword
-    displayPassword4.textContent = fourthPassword
 }
 
+passwordElements.forEach((passwordElement) => {
+    passwordElement.addEventListener("click", () => {
+        const password = passwordElement.textContent;
+
+        navigator.clipboard
+            .writeText(password)
+            .then(() => {
+                // Success
+                alert("Password copied to clipboard!");
+            })
+            .catch((error) => {
+                // Error
+                console.error("Failed to copy password:", error);
+            });
+    });
+});
